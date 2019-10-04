25b8f423b314ec62c2025d67b8f0454b4629edd9
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:26:45 +0200

    Add project files.

---
+++ LibreriaMatematica/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LibreriaMatematica
+{
+    public class Operazioni
+    {
+        public static int Somma(int n1, int n2)
+        {
+            int s = n1 + n2;
+            return s;
+        }
+        public static int Differenza(int n1, int n2)
+        {
+            int d = n1 - n2;
+            return d;
+        }
+        public static int Prodotto(int n1, int n2)
+        {
+            int p = n1 * n2;
+            return p;
+        }
+        public static double Divisione(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                throw new Exception("n2 = 0");
+            }
+            double q = n1 / n2;
+            return q;
+        }
+        public static double Massimo(int n1, int n2)
+        {
+            if (n1 > n2)
+            {
+                double max = n1;
+                return max;
+            }
+            else
+            {
+                double max = n2;
+                return max;
+            }
+        }
+        public static double Minimo(int n1, int n2)
+        {
+            if (n1 < n2)
+            {
+                double min = n1;
+                return min;
+            }
+            else
+            {
+                double min = n2;
+                return min;
+            }
+        }
+        public static double Potenza(int n1, int n2)
+        {
+            int c = 0;
+            int p = 1;
+            while (n2 < c)
+            {
+                p = p * n1;
+                c = c + 1;
+            }
+            return p;
+        }
+    }
+}
---
+++ Matematica/Program.cs
@@ -0,0 +1,48 @@
+﻿using LibreriaMatematica;
+using System;
+
+namespace Matematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il primo numero");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il secondo numero");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("che operazione vuoi fare? Somma / Sottrazione / Moltiplicazione / Divisione / Massimo / Minimo / Potenza");
+            string ris= Console.ReadLine();
+            double risultato = 0;
+            switch (ris)
+            {
+                case "Somma":
+                    risultato = Operazioni.Somma(n1, n2);
+                    break;
+                case "Prodotto":
+                    risultato = Operazioni.Prodotto(n1, n2);
+                    break;
+                case "Differenza":
+                    risultato = Operazioni.Differenza(n1, n2);
+                    break;
+                case "Divisione":
+                    risultato = Operazioni.Divisione(n1, n2);
+                    break;
+                case "Massimo":
+                    risultato = Operazioni.Massimo(n1, n2);
+                    break;
+                case "Minimo":
+                    risultato = Operazioni.Minimo(n1, n2);
+                    break;
+                case "Potenza":
+                    risultato = Operazioni.Potenza(n1, n2);
+                    break;
+                default:
+                    throw new Exception(" scelta non valida");
+            }
+            Console.WriteLine($"{risultato}");
+            Console.ReadLine();
+            }
+        }
+    }
+
